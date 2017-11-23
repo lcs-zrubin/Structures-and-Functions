dbe29cf54bbd7ebb65418c5a677776e43cc10433
Russell Gordon <rgordon@lcs-mba2410.lcs.on.ca>
2017-11-23 09:09:29 -0500

    Added exercise

---
+++ Structures and Functions.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+/*:
+ [Previous](@previous)
+ 
+ The following statement is required to make the playground run.
+ 
+ 
+ Please do not remove.
+ */
+import Foundation
+/*:
+ ## Exercise
+ 
+ Write a function that determines the shortest distance from a point on a Cartesian plane to a line on a Cartesian plane.
+ 
+ You can re-use the **Point** structure and the *distance* function.
+ 
+ You should declare a **Line** structure.
+ 
+ Recall that the shortest distance from a point to a line is the perpendicular distance.
+
+*/
+
+// Begin here... once you teach the computer to do this, you will never need to do it by hand again! :)
