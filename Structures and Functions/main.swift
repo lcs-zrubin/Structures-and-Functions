466c882afb10faa1a425b14266ca24bf55778828
Russell Gordon <rgordon@lcs.on.ca>
2017-11-23 07:07:06 -0500

    Initial Commit

---
+++ Structures and Functions/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Structures and Functions
+//
+//  Created by Russell Gordon on 11/23/17.
+//  Copyright © 2017 Russell Gordon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
