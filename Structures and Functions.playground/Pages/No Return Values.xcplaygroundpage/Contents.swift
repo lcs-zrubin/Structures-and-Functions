ca0c27749bae057ca290d6690b77bb24a9b29335
Russell Gordon <rgordon@lcs.on.ca>
2017-11-23 07:33:26 -0500

    Page 2, functions with no return values, complete.

--- Structures and Functions.playground/Pages/No Return Values.xcplaygroundpage/Contents.swift
+++ Structures and Functions.playground/Pages/No Return Values.xcplaygroundpage/Contents.swift
@@ -16,13 +16,45 @@ import Foundation
  
  Say we want to greet a person. We could define the following function:
  
- ![example](example.png "example")
+ ![example](example2.png "example")
  
  Type the code that you see above. Then run the playground to try it out.
  */
+// Define the function here
+
+
+// Now greet Dave by invoking the function
+
 
 /*:
+ As shown in the screenshot above, the code on lines 1 through 3 **define** the function.
  
+ Line 1 is the *function header*.
+ 
+ Line 2 is the *function body* which is everything that is between the opening and closing curly brackets { }.
+ 
+ Line 3 is the closing curly bracket that marks the end of the function.
+ 
+ Line 4 is where the function is *called* or *invoked*.
+ 
+ Add additional lines of code to invoke the function and greet five of your friends in this class.
+ 
+ */
+// Invoke the function many more times to greet five friends
+
+/*:
+ By encapsulating behaviour of a program inside a function, it is easier to maintain.
+ 
+ Say you are very pleased to see your friend Dave and your other five friends.
+ 
+ *Modify* the behaviour of the function you defined above, so that when invoked, it says:
+ 
+     Hello, Dave! A sincere pleasure to meet you on this very fine day.
+ 
+ Now run your playground again.
+ 
+ Notice how you changed one line of code inside the function body, but, the behaviour is changed for each and every time you use, or invoke, the function.
+ */
 
 /*:
  [Next](@next)
