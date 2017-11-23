c19a4a2e2b9e56a0a64f4f1ef1490a2b6dfc854b
Russell Gordon <rgordon@lcs.on.ca>
2017-11-23 07:23:47 -0500

    Intro complete. Working on functions with no return values.

---
+++ Structures and Functions.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+/*:
+ # Introduction
+ 
+ Functions are useful because they help us:
+ 
+ * implement an abstract rule (i.e.: draw an n-sided regular polygon)
+ * better organize our code by making it more readable
+ * make code easier to maintain (this is called **refactoring** our code)
+ 
+ This playground will recap how basic functions work, how functions that return values work, and how **structures** can make our functions even more useful.
+ 
+ Ready? Let's begin.
+ 
+ [Next](@next)
+ 
+ */
+//: NOTE: Parts of this multi-page playground are adapted from [The Swift Programming Language Guide - Functions](https://developer.apple.com/library/content/documentation/Swift/Conceptual/Swift_Programming_Language/Functions.html#//apple_ref/doc/uid/TP40014097-CH10-ID158).
+
---
+++ Structures and Functions.playground/Pages/No Return Values.xcplaygroundpage/Contents.swift
@@ -0,0 +1,29 @@
+/*:
+ [Previous](@previous)
+ 
+ The following statement is required to make the playground run.
+ 
+ 
+ Please do not remove.
+ */
+import Foundation
+/*:
+ ## Functions
+ ### Functions with no return values
+ Functions do not always need to return a value.
+ 
+ Sometimes it is helpful just to *encapsulate* some behaviour inside a function to make code more readable.
+ 
+ Say we want to greet a person. We could define the following function:
+ 
+ ![example](example.png "example")
+ 
+ Type the code that you see above. Then run the playground to try it out.
+ */
+
+/*:
+ 
+
+/*:
+ [Next](@next)
+ */
